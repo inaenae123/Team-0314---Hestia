9ed03e345228bd4926e15fe1bdb7e0be9b350a8e
crystal <cli784@yahoo.com>
2021-02-03 08:53:15 -1000

    added in the backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+// connect to db
+dotenv.config();
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+},
+() => console.log("connected to db")
+);
+// import routes
+const authRoutes = require("./routes/auth");
+
+// middlewares
+app.use(express.json()); // for body parser
+
+// route middlewares
+app.use("/api/user", authRoutes);
+app.listen(3000, () => console.log("server is running..."));
