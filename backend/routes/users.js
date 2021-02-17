b176c63a12def9871ef49c3fbbec8f573108f34c
rduchesne112358 <rduchesne@gatech.edu>
2021-02-17 14:18:25 -0500

    Revert "Update users.js"
    This reverts commit 804f116c04efe3e3525f0b09def81353641d26ef.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,7 +9,7 @@ const validateLoginInput = require("../validation/login");
 const User = require("../model/User");
 
 
-router.get("/api/user", async (req, res) => {
+router.get("/user", async (req, res) => {
   console.log('hello world')
 });
 
